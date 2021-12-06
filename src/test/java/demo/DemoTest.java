318715944a4f252da7c6529d6bcd61d94a6fc3b2
feigeswjtu <feigeswjtu@163.com>
2021-12-06 08:33:08 +0800

    first commit

---
+++ src/test/java/demo/DemoTest.java
@@ -0,0 +1,8 @@
+package demo;
+
+/**
+ * @author feigeswjtu.cyf
+ * @version $Id: DemoTest.java, v 0.1 2021-12-05 13:13 feigeswjtu.cyf Exp $$
+ */
+public class DemoTest {
+}
---
+++ src/test/java/demo/DemoV1Test.java
@@ -0,0 +1,8 @@
+package demo;
+
+/**
+ * @author feigeswjtu.cyf
+ * @version $Id: DemoV1Test.java, v 0.1 2021-12-05 13:13 feigeswjtu.cyf Exp $$
+ */
+public class DemoV1Test {
+}
